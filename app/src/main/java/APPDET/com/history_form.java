3124e55bc25eaca991d2ff547f6151583531e7c4
BenedictoLucido <benedictlucido@gmail.com>
2022-05-19 19:25:20 +0800

    ADDED NEW FORM! History Account Edit Account

---
+++ app/src/main/java/APPDET/com/history_form.java
@@ -0,0 +1,14 @@
+package APPDET.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class history_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_form);
+    }
+}
