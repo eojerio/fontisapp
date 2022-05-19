619778a035d1b0d60c7c6f0b17d81bf3cfd19395
itlognamaalat21 <rivennation201@gmail.com>
2022-05-19 19:15:22 +0800

    Switching beta test home -> cart - (OJERIO)

--- app/src/main/java/APPDET/com/cart_form.java
+++ app/src/main/java/APPDET/com/cart_form.java
@@ -2,13 +2,34 @@ package APPDET.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class cart_form extends AppCompatActivity {
+    ImageButton btnHome, btnHistory;
+    Intent intentHomeCART, intentHistoryCART;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_form);
+
+        homeCART();
+    }
+
+    //going to index page
+    public void homeCART(){
+        btnHome = (ImageButton) findViewById(R.id.btnHome);
+        intentHomeCART = new Intent(this, index_form.class);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentHomeCART);
+            }
+        });
     }
 }
--- app/src/main/java/APPDET/com/index_form.java
+++ app/src/main/java/APPDET/com/index_form.java
@@ -2,14 +2,36 @@ package APPDET.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class index_form extends AppCompatActivity {
+    ImageButton btnHome, btnCart;
+    Intent intentHome, intentCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index_form);
 
+        cart();
     }
+
+    //going to cart page
+    public void cart(){
+        btnCart = (ImageButton) findViewById(R.id.btnCart);
+        intentCart = new Intent (this, cart_form.class);
+
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentCart);
+            }
+        });
+    }
+
+
 }
