92952de602c7c98867291a5675f40b60d88a6811
BenedictoLucido <benedictlucido@gmail.com>
2022-05-25 09:51:56 +0800

    Account page FUNCTION STARTED PART 1.1 (TESTING) LUCICO JR. 24/05/2022

--- app/src/main/java/APPDET/com/cartFragment.java
+++ app/src/main/java/APPDET/com/cartFragment.java
@@ -1,11 +1,8 @@
 package APPDET.com;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -86,7 +83,7 @@ public class cartFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_cart, container, false);
 
-        tvCart = (TextView) v.findViewById(R.id.tvCartTest);
+        tvCart = (TextView) v.findViewById(R.id.tvAddress);
 
         strtext = getArguments().getString("data");
 
