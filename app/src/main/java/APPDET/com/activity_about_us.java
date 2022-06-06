8107955c6ba88908e684c709857e6edad61b8e3f
BenedictoLucido <benedictlucido@gmail.com>
2022-06-06 09:34:41 +0800

    about us part 1 line comments @ fragment_account androidmanifest account_fragment
    Lucido Jr. 9:34am 06/06/2022

--- app/src/main/java/APPDET/com/activity_about_us.java
+++ app/src/main/java/APPDET/com/activity_about_us.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_about extends AppCompatActivity {
+public class activity_about_us extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_about_us);
     }
 }
