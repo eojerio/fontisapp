cddffb067f51024beaf9313e17c2b2c6a333afd9
BenedictoLucido <benedictlucido@gmail.com>
2022-06-06 17:19:45 +0800

    previous button about us,
    Lucido Jr. 3:27pm 06/06/2022

--- app/src/main/java/APPDET/com/activity_about_us.java
+++ app/src/main/java/APPDET/com/activity_about_us.java
@@ -2,7 +2,9 @@ package APPDET.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_about_us extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class activity_about_us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+    public void previous(View view){
+        finish();
     }
 
     //test
