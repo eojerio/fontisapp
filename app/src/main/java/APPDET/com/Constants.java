beb9c6a4c0c3a57ae5d171126bb682e27c127331
BenedictoLucido <benedictlucido@gmail.com>
2022-06-08 14:49:54 +0800

    about_us margins fragment_account icons, button colors, constraints, margins, image loc. fragment_account_edit button colors homeFragment *** prod id ***
    Lucido Jr. 2:49pm 08/06/2022

--- app/src/main/java/APPDET/com/Constants.java
+++ app/src/main/java/APPDET/com/Constants.java
@@ -3,7 +3,7 @@ package APPDET.com;
 public class Constants {
 
     //IPv4 database connection
-    private static final String ROOT_URL = "http://192.168.254.103/APPDET_ANDROIDBOTS_FONTISAPP/fontisDB/v1/";
+    private static final String ROOT_URL = "http://192.168.100.170/APPDET_ANDROIDBOTS_FONTISAPP/fontisDB/v1/";
 
     //file constant php connections
     public static final String URL_REGISTER = ROOT_URL + "register.php";
