5c91ca6ec8b759d97ff5f994495d4934bd8bded6
itlognamaalat21 <rivennation201@gmail.com>
2022-06-10 13:33:51 +0800

    ADMIN PROCESSSES PT.15 WITH ADDED PICTURES OF PRODUCTS  09/06/2022. - (OJERIO)

--- app/src/main/java/APPDET/com/Constants.java
+++ app/src/main/java/APPDET/com/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     //IPv4 database connection
     private static final String ROOT_URL = "http://192.168.254.103/APPDET_ANDROIDBOTS_FONTISAPP/fontisDB/v1/";
 
-    //file constant php connections
+    //file constant php connections 
     public static final String URL_REGISTER = ROOT_URL + "register.php";
     public static final String URL_LOGIN = ROOT_URL + "login.php";
     public static final String URL_ADDCART = ROOT_URL + "add_cart.php";
